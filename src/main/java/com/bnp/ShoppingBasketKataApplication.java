42214e3929f97473be6907e7b50803e8f571a001
skjavab <srshryls@gamil.com>
2022-04-02 09:32:46 +0530

    Add spring boot dependencies

---
+++ src/main/java/com/bnp/ShoppingBasketKataApplication.java
@@ -0,0 +1,13 @@
+package com.bnp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingBasketKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingBasketKataApplication.class, args);
+	}
+
+}
