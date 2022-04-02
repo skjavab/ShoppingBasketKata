ec6c4ef1b002a1632b496803685623775af4bae2
skjavab <srshryls@gamil.com>
2022-04-02 11:40:35 +0530

    Additional test cases : normal price when buy three or more same books

--- src/test/java/com/bnp/services/BookCartServiceImplTest.java
+++ src/test/java/com/bnp/services/BookCartServiceImplTest.java
@@ -33,4 +33,27 @@ public class BookCartServiceImplTest {
 		Assertions.assertEquals(Double.valueOf("100.0"), actualPrice);
 	}
 
+	@Test
+	public void normalPriceWhenBuyMoreSameBook() {
+		double actualPrice = 0.0;
+		ShoppingCartItem shoppingCartItem = null;
+		shoppingCartItem = new ShoppingCartItem();
+		shoppingCartItem.setBookId(Integer.valueOf("1"));
+		shoppingCartItem.setQuantity(Integer.valueOf("3"));
+		actualPrice = service.calculatePrice(shoppingCartItem);
+		Assertions.assertEquals(Double.valueOf("150.0"), actualPrice);
+		shoppingCartItem = new ShoppingCartItem();
+		shoppingCartItem.setBookId(Integer.valueOf("1"));
+		shoppingCartItem.setQuantity(Integer.valueOf("4"));
+		actualPrice = service.calculatePrice(shoppingCartItem);
+		Assertions.assertEquals(Double.valueOf("200.0"), actualPrice);
+		actualPrice = service.calculatePrice(shoppingCartItem);
+		shoppingCartItem = new ShoppingCartItem();
+		shoppingCartItem.setBookId(Integer.valueOf("1"));
+		shoppingCartItem.setQuantity(Integer.valueOf("5"));
+		actualPrice = service.calculatePrice(shoppingCartItem);
+		Assertions.assertEquals(Double.valueOf("250.0"), actualPrice);
+
+	}
+
 }
