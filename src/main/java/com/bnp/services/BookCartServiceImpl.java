b333862c543e1751c1079b20dd935fc34fd0d7d2
skjavab <srshryls@gamil.com>
2022-04-02 12:26:02 +0530

    5 percentage discount price when buy two different books

--- src/main/java/com/bnp/services/BookCartServiceImpl.java
+++ src/main/java/com/bnp/services/BookCartServiceImpl.java
@@ -1,6 +1,8 @@
 
 package com.bnp.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.bnp.model.ShoppingCartItem;
@@ -8,8 +10,20 @@ import com.bnp.model.ShoppingCartItem;
 @Service
 public class BookCartServiceImpl {
 
-	public double calculatePrice(ShoppingCartItem shoppingCartItem) {
-		return 50.0 * shoppingCartItem.getQuantity();
+	public double calculatePrice(List<ShoppingCartItem> bookList) {
+		double calculatedPrice = 0d;
+		if (bookList != null) {
+			if (bookList.size() == 2) {
+				double basePrice = bookList.size() * 50;
+				double discountedPriceForSet = basePrice - ((basePrice * 5) / 100.0);
+				calculatedPrice += discountedPriceForSet;
+			}
+		} else {
+			ShoppingCartItem shoppingCartItembook = bookList.get(0);
+			return 50.0 * shoppingCartItembook.getQuantity();
+		}
+
+		return calculatedPrice;
 
 	}
 
--- src/test/java/com/bnp/services/BookCartServiceImplTest.java
+++ src/test/java/com/bnp/services/BookCartServiceImplTest.java
@@ -1,5 +1,8 @@
 package com.bnp.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,43 +20,71 @@ public class BookCartServiceImplTest {
 
 	@Test
 	public void normalPriceWhenBuyOneBook() {
+		List<ShoppingCartItem> listshoppingCartItem = new ArrayList<ShoppingCartItem>();
 		ShoppingCartItem shoppingCartItem = new ShoppingCartItem();
 		shoppingCartItem.setBookId(Integer.valueOf("1"));
 		shoppingCartItem.setQuantity(Integer.valueOf("1"));
-		double actualPrice = service.calculatePrice(shoppingCartItem);
+		listshoppingCartItem.add(shoppingCartItem);
+		double actualPrice = service.calculatePrice(listshoppingCartItem);
 		Assertions.assertEquals(Double.valueOf("50.0"), actualPrice);
 	}
 
 	@Test
 	public void normalPriceWhenBuyTwoSameBook() {
+		List<ShoppingCartItem> listshoppingCartItem = new ArrayList<ShoppingCartItem>();
 		ShoppingCartItem shoppingCartItem = new ShoppingCartItem();
 		shoppingCartItem.setBookId(Integer.valueOf("1"));
 		shoppingCartItem.setQuantity(Integer.valueOf("2"));
-		double actualPrice = service.calculatePrice(shoppingCartItem);
+		listshoppingCartItem.add(shoppingCartItem);
+		double actualPrice = service.calculatePrice(listshoppingCartItem);
 		Assertions.assertEquals(Double.valueOf("100.0"), actualPrice);
 	}
 
 	@Test
 	public void normalPriceWhenBuyMoreSameBook() {
+		List<ShoppingCartItem> listshoppingCartItem = null;
+		listshoppingCartItem = new ArrayList<ShoppingCartItem>();
 		double actualPrice = 0.0;
 		ShoppingCartItem shoppingCartItem = null;
 		shoppingCartItem = new ShoppingCartItem();
 		shoppingCartItem.setBookId(Integer.valueOf("1"));
 		shoppingCartItem.setQuantity(Integer.valueOf("3"));
-		actualPrice = service.calculatePrice(shoppingCartItem);
+		listshoppingCartItem.add(shoppingCartItem);
+		actualPrice = service.calculatePrice(listshoppingCartItem);
+
+		listshoppingCartItem = new ArrayList<ShoppingCartItem>();
 		Assertions.assertEquals(Double.valueOf("150.0"), actualPrice);
 		shoppingCartItem = new ShoppingCartItem();
 		shoppingCartItem.setBookId(Integer.valueOf("1"));
 		shoppingCartItem.setQuantity(Integer.valueOf("4"));
-		actualPrice = service.calculatePrice(shoppingCartItem);
+		listshoppingCartItem.add(shoppingCartItem);
+		actualPrice = service.calculatePrice(listshoppingCartItem);
 		Assertions.assertEquals(Double.valueOf("200.0"), actualPrice);
-		actualPrice = service.calculatePrice(shoppingCartItem);
+
+		listshoppingCartItem = new ArrayList<ShoppingCartItem>();
 		shoppingCartItem = new ShoppingCartItem();
 		shoppingCartItem.setBookId(Integer.valueOf("1"));
 		shoppingCartItem.setQuantity(Integer.valueOf("5"));
-		actualPrice = service.calculatePrice(shoppingCartItem);
+		listshoppingCartItem.add(shoppingCartItem);
+		actualPrice = service.calculatePrice(listshoppingCartItem);
 		Assertions.assertEquals(Double.valueOf("250.0"), actualPrice);
 
+	}
+
+	@Test
+	public void fivePriceDiscountWhenBuytwoDifferentBook() {
+		List<ShoppingCartItem> listshoppingCartItem = new ArrayList<ShoppingCartItem>();
+		ShoppingCartItem shoppingCartItem = new ShoppingCartItem();
+		shoppingCartItem.setBookId(Integer.valueOf("1"));
+		shoppingCartItem.setQuantity(Integer.valueOf("1"));
+		listshoppingCartItem.add(shoppingCartItem);
+
+		ShoppingCartItem shoppingCartItem1 = new ShoppingCartItem();
+		shoppingCartItem1.setBookId(Integer.valueOf("2"));
+		shoppingCartItem1.setQuantity(Integer.valueOf("1"));
+		listshoppingCartItem.add(shoppingCartItem1);
+		double actualPrice = service.calculatePrice(listshoppingCartItem);
+		Assertions.assertEquals(Double.valueOf("95.0"), actualPrice);
 	}
 
 }
